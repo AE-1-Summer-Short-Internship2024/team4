acfe6b31bb66e2601ef15c2fce350ca0b4b17759
shiv-ko <iscopre7@gmail.com>
2024-08-06 13:14:54 +0900

    add: product component+routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 // frontend/src/App.js
 import React, { useEffect, useState } from 'react';
 import Header from './Header/Header';
+import Product from './compoents/main/Product';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
   
-
   return (
     <div>
       <Header />
       <h1>災害備蓄アプリ</h1>
+
+    {/* ルーティングの設定 */}
+      <Router>  
+      <div className="app-content">
+        <Routes>
+          {/* path：/home　でProductコンポネントに飛ばす。 */}
+          <Route path="/home" element={<Product />} />
+
+  
+        </Routes>
+      </div>
+    </Router>
     </div>
   );
 }
---
+++ src/compoents/main/Product.js
@@ -0,0 +1,17 @@
+// frontend/src/App.js
+import React, { useEffect, useState } from 'react';
+
+
+
+
+function Product() {
+  return (
+    <div>
+      
+      <h1>this is product.js</h1>
+      
+    </div>
+  );
+}
+
+export default Product;
