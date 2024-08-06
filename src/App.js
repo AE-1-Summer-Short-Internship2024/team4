4530b6877ec1e6ff7aae122c02dc4a913acb2c74
tyowa0111 <ito.taichi0111@gmail.com>
2024-08-06 23:40:18 +0900

    マージ

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 // frontend/src/App.js
 import React, { useEffect, useState } from 'react';
 import Header from './Header/Header';
-import ProductList from './compoents/product/ProductList';
-import AuthComponent from './compoents/Login/components/AuthComponent';
-import Login_app from './compoents/Login/Login_app';
- 
-
+import Product from './compoents/product/ProductList';
+import InventoryControl from './compoents/InventoryControl/InventoryControl';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
-import UserRegist from './compoents/main/UserRegist';
 
 
 function App() {
@@ -19,18 +15,16 @@ function App() {
   return (
     <div>
       <Header />
-      
 
     {/* ルーティングの設定 */}
       <Router>  
       <div className="app-content">
         <Routes>
           {/* path：/home　でProductコンポネントに飛ばす。 */}
-          <Route path="/home" element={<ProductList />} />
-          <Route path="/Login" element={<Login_app />} />
-          <Route path="/userregist" element={<UserRegist />} />
-
+          <Route path="/home" element={<Product />} /> 
+          <Route path="/inventory"  element={<InventoryControl />} />
         </Routes>
+
       </div>
     </Router>
     </div>
